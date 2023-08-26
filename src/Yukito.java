6e617c43a27fb51b8c559e6588b6b85be8fa975f
Alexa <za0ne78@hotmail.com>
2023-08-27 00:05:47 +0300

    One of my favorite japaneese stories

---
+++ src/Yukito.java
@@ -0,0 +1,10 @@
+/**
+ *
+ *  Yuko Ichihara (壱原 侑子, Ichihara Yūko), also known as the Dimensional Witch or the Space-Time Witch,
+ *  is a human being which knows chemistry well and is as good as an alchemist and then the dimentional
+ *  witch story was invented to tell the tales of her code, and she was once called "Alexandra Cotoc" and then
+ *  "Alexandra Martinez Joya"
+ */
+
+public class Yukito {
+}
