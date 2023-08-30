bb5af21deef7ecbc13ffd5369eb5f0b264e477ec
Alexa <za0ne78@hotmail.com>
2023-08-30 22:17:30 +0300

    Created Hearts From Arcs

---
+++ src/CreateHeartsFromArcs.java
@@ -0,0 +1,21 @@
+import acm.graphics.GPolygon;
+import acm.program.GraphicsProgram;
+
+public class CreateHeartsFromArcs  extends GraphicsProgram {
+
+        public void run() {
+                double diamondSide = 100;
+                int angle = -45;
+                int angle2 = 180;
+                GPolygon heart = new GPolygon();
+
+                angle += 90;
+                heart.addPolarEdge(diamondSide, angle);
+                angle += 90;
+                heart.addPolarEdge(diamondSide, angle);
+                heart.addArc(diamondSide, diamondSide, -angle ,  -angle2);
+                heart.addArc(diamondSide, diamondSide, angle, -angle2);
+
+                add(heart , getWidth() / 2, getHeight() / 2);
+        }
+}
