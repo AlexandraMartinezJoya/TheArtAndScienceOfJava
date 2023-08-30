aa2d01e8a6987b5e0faaac50de035b94e04c9dcd
Alexa <za0ne78@hotmail.com>
2023-08-31 01:04:50 +0300

    Glowing Rainbow Heart

--- src/DrawHeartExtended.java
+++ src/DrawHeartExtended.java
@@ -1,5 +1,4 @@
 import acm.graphics.GArc;
-import acm.graphics.GCompound;
 import acm.graphics.GPolygon;
 import acm.program.GraphicsProgram;
 
@@ -16,33 +15,36 @@ public class DrawHeartExtended extends GraphicsProgram {
     private class FilledHeart extends GPolygon {
 
         public FilledHeart(double initialStartPointXCoord, double initialStartPointYCoord) {
-            GPolygon diamond = new GPolygon();
-            diamond.addVertex( - DIAMOND_SIDE , 0 );
-            diamond.addVertex(  0, DIAMOND_HEIGHT  );
-            diamond.addVertex( DIAMOND_SIDE , 0 );
-            diamond.addVertex(  0, - DIAMOND_SIDE  );
-//            diamond.addArc( DIAMOND_SIDE *2  , DIAMOND_SIDE * 2 , 0,  90 );
-            diamond.addArc( DIAMOND_SIDE , 90, 90, 10  );
-//            diamond.addArc(100/ 2, 45, 90, 100  );
-//            diamond.addArc(100,90, 45, 0  );
-//            diamond.setFilled(true);
-//            diamond.setFillColor(Color.RED);
-            add(diamond, initialStartPointXCoord , initialStartPointYCoord );
-            GArc heartRightSide = new GArc(DIAMOND_SIDE * 2.1, DIAMOND_SIDE * 2.1,  0, 88);
-//            heartRightSide.setFilled(true);
-//            heartRightSide.setFillColor(Color.RED);
-            add(heartRightSide, initialStartPointXCoord - DIAMOND_SIDE - DIAMOND_SIDE / 10 , initialStartPointYCoord - DIAMOND_SIDE - DIAMOND_SIDE / 20 );
+            double  r = DIAMOND_SIDE;
+            GPolygon newDiamond = new GPolygon();
+            newDiamond.addVertex( -DIAMOND_SIDE * 4, DIAMOND_SIDE  * 4  );
+            int angle = -45;
+            for(int i = 0; i < 4; i ++){
+                newDiamond.addPolarEdge(DIAMOND_SIDE, angle);
+                angle += 90;
+            }
+            newDiamond.setFilled(true);
+            newDiamond.setFillColor(Color.RED);
+            newDiamond.setColor(Color.RED);
+            add(newDiamond, initialStartPointXCoord  * 1.88   , initialStartPointYCoord - initialStartPointYCoord * 2 + 1.44 * DIAMOND_SIDE );
 
-            GArc heartLeftSide = new GArc(DIAMOND_SIDE *2, DIAMOND_SIDE * 2, 30, 150 );
-            add(heartLeftSide, initialStartPointXCoord -   DIAMOND_SIDE ,  initialStartPointYCoord - DIAMOND_SIDE);
+            GArc heartRightOutline1 = new GArc( r,  r  , -45, 180);
+            heartRightOutline1.setFilled(true);
+            heartRightOutline1.setFillColor(Color.RED);
+            heartRightOutline1.setColor(Color.RED);
+            add(heartRightOutline1,initialStartPointXCoord  - DIAMOND_SIDE / 8 , initialStartPointYCoord  - DIAMOND_SIDE /10 );
 
+            GArc heartLeftOutline = new GArc( r  ,  r , 45, 180);
+            heartLeftOutline.setFilled(true);
+            heartLeftOutline.setFillColor(Color.RED);
+            heartLeftOutline.setColor(Color.RED);
+            add(heartLeftOutline, initialStartPointXCoord - DIAMOND_SIDE + DIAMOND_SIDE / 6, initialStartPointYCoord - DIAMOND_SIDE / 6 + DIAMOND_SIDE / 14);
         }
 
         /**
          * Private constants
          */
         private final double DIAMOND_SIDE = 100;
-        private final double DIAMOND_HEIGHT = 100;
     }
 
 }
---
+++ src/GlowingRainbowHeart.java
@@ -0,0 +1,32 @@
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+public class GlowingRainbowHeart extends GraphicsProgram {
+
+    public void run() {
+        GHeart heart = new GHeart(HEART_SIZE_M);
+        heart.setFilled(true);
+        add(heart, (getWidth() -  HEART_SIZE_M) /2 , (getHeight() + 2 * HEART_SIZE_M)  / 2);
+        while(true) {
+            heart.setColor(rgen.nextColor());
+            pause(PAUSE_TIME);
+        }
+
+    }
+
+
+    /** Pause time in milliseconds **/
+    private final double PAUSE_TIME = 1000;
+
+    /**Random number generator */
+    private RandomGenerator rgen = new RandomGenerator();
+
+
+    private GHeart heart;
+
+    private final double HEART_SIZE_M = 75;
+
+    private final double HEART_SIZE_L = 100;
+
+    private final double HEART_SIZE_S = 50;
+}
